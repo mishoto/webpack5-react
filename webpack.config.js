a24c2a30bda73c9e06ca4ecfde227d1acd12c7dc
mishoto <mihail.dev.io@gmail.com>
2022-06-08 15:32:29 +0300

    create webpack.config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+let DEV_TOOL = null;
+
+if (process.env.NODE_ENV === "production") {
+  mode = "production";
+  DEV_TOOL = "source-map";
+}
+if (process.env.NODE_ENV === "development") {
+  mode = "development";
+  DEV_TOOL = "eval";
+}
+
+module.exports = {
+  mode: "production",
+  devtool: "source-map",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "./dist"),
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+  plugins: [new MiniCssExtractPlugin()],
+};
