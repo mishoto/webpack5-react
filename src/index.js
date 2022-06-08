1495f09df39667be10b2c07bd2268e4ba5fee84f
mishoto <mihail.dev.io@gmail.com>
2022-06-08 15:31:53 +0300

    add second object

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
+import "../src/index.css";
+
 const recipe = {
   apple: 2,
   mango: 4,
   orange: 4,
 };
 
+const recipe01 = {
+  ...recipe,
+  juice: 3,
+  mango: 2,
+  banana: 1,
+};
+
 console.log(recipe);
+console.log(recipe01);
